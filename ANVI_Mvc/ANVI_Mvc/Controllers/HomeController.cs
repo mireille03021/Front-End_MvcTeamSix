e09af7d8f893d0605aaff9243e7b6ca1aae4015c
chia yun <ywoei0522@gmail.com>
2019-05-29 21:19:28 +0800

    認可

--- ANVI_Mvc/ANVI_Mvc/Controllers/HomeController.cs
+++ ANVI_Mvc/ANVI_Mvc/Controllers/HomeController.cs
@@ -202,6 +202,7 @@ namespace ANVI_Mvc.Controllers
             ViewData["Address"] = OCVM.Address;
             ViewData["Phone"] = OCVM.Phone;
             ViewData["Email"] = OCVM.Email;
+            ViewData.Model = OCVM;
             return View();
         }
 
